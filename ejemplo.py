baf5597cdf90b7fc3bd95a13c212a39f90210898
Yinyer Reyes <reyesyinyer@gmail.com>
2024-06-27 19:28:19 -0400

    se añaden dos nuevas funciones multiplicar y dividir

--- ejemplo.py
+++ ejemplo.py
@@ -1,4 +1,9 @@
 def restar(n1, n2):
     print("el resultado de la resta entre n1 - n2: ", n1 - n2)
     
-sumar(int(input("ingresa el primer numero: ")), int(input("ingresa el segundo numero: ")))
+restar(int(input("ingresa el primer numero: ")), int(input("ingresa el segundo numero: ")))
+
+def multiplicar(n1, n2):
+    print("el resultado de la multiplicacion entre n1 * n2: ", n1 * n2)
+    
+multiplicar(int(input("ingresa el primer numero: ")), int(input("ingresa el segundo numero: ")))
--- ejemplo1.py
+++ ejemplo1.py
@@ -7,4 +7,9 @@ def sumar(n1, n2):
     print("el resultado de la suma entre n1 + n2: ", n1 + n2)
     
 sumar(int(input("ingresa el primer numero: ")), int(input("ingresa el segundo numero: ")))
+
+def division(n1, n2):
+    print("el resultado de la division entre n1 / n2: ", n1 / n2)
+    
+division(int(input("ingresa el primer numero: ")), int(input("ingresa el segundo numero: ")))
     
